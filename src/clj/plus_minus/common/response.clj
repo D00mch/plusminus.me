f33bd53f2d5adf09ce631c588b283e8055a6055b
liverm0r <arturdumchev@yandex.ru>
2019-08-12 20:21:31 +0300

    reused code from common/response

--- src/clj/plus_minus/common/response.clj
+++ src/clj/plus_minus/common/response.clj
@@ -2,11 +2,12 @@
   (:require [ring.util.http-response :as response]
             [clojure.tools.logging :as log]))
 
-(defn e-precondition [msg]
+(defn e-precondition [msg & [data]]
   (log/error msg)
-  (response/precondition-failed {:result :error, :message msg}))
+  (response/precondition-failed (merge {:result :error, :message msg} data)))
 
 (defn e-internal [e msg]
+  (log/error e msg)
   (response/internal-server-error {:result :error, :message msg}))
 
 (defn try-with-wrap-internal-error [& {:keys [fun msg]}]
