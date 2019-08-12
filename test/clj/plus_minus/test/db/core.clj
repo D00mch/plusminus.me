479b9303b1ac34cc2e14116d23a8ccfff3eeb517
liverm0r <arturdumchev@yandex.ru>
2019-08-12 22:50:37 +0300

    fix inserting user in db test

--- test/clj/plus_minus/test/db/core.clj
+++ test/clj/plus_minus/test/db/core.clj
@@ -2,12 +2,12 @@
   (:require
     [plus-minus.db.core :refer [*db*] :as db]
     [luminus-migrations.core :as migrations]
-    [clojure.test :refer :all]
+    [clojure.test :refer [use-fixtures deftest is testing]]
     [clojure.java.jdbc :as jdbc]
     [plus-minus.config :refer [env]]
     [mount.core :as mount]))
 
-#_(use-fixtures
+(use-fixtures
   :once
   (fn [f]
     (mount/start
@@ -16,22 +16,17 @@
     (migrations/migrate ["migrate"] (select-keys env [:database-url]))
     (f)))
 
-#_(deftest test-users
+(deftest test-users
   (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
     (is (= 1 (db/create-user!
                t-conn
                {:id         "1"
-                :first_name "Sam"
-                :last_name  "Smith"
-                :email      "sam.smith@example.com"
                 :pass       "pass"})))
-    (is (= {:id         "1"
-            :first_name "Sam"
-            :last_name  "Smith"
-            :email      "sam.smith@example.com"
-            :pass       "pass"
-            :admin      nil
-            :last_login nil
-            :is_active  nil}
+    (is (= {:id        "1",
+            :email      nil,
+            :admin      nil,
+            :last_login nil,
+            :is_active  nil,
+            :pass      "pass"}
            (db/get-user t-conn {:id "1"})))))
