4b93db2bbabc202a34e6ede0d44f570012c9551b
liverm0r <arturdumchev@yandex.ru>
2019-08-26 23:41:18 +0300

    cascade delete all tables depending on users.id

--- resources/migrations/20190618045631-add-single-game-table.up.sql
+++ resources/migrations/20190618045631-add-single-game-table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE game_states (
  player_id VARCHAR(20) PRIMARY KEY NOT NULL,
- state     JSON
+ state     JSON,
+ FOREIGN KEY (player_id) REFERENCES users(id) ON DELETE CASCADE
 );
 --;;
 CREATE TABLE game_statistics (
  player_id  VARCHAR(20) PRIMARY KEY NOT NULL,
- statistics JSON
+ statistics JSON,
+ FOREIGN KEY (player_id) REFERENCES users(id) ON DELETE CASCADE
 );
