3145d2934a4afe906d1d047781151d1118477f6c
liverm0r <arturdumchev@yandex.ru>
2019-08-28 11:24:35 +0300

    update head theme, cache icons

--- resources/public/js/sw.js
+++ resources/public/js/sw.js
@@ -6,7 +6,15 @@ var filesToCache = [
     '/html/error.html',
     '/css/screen.css',
     '/img/warning_clojure.png',
-    '/img/google.png'
+    '/img/google.png',
+    '/img/icons/icon-72x72.png',
+    '/img/icons/icon-96x96.png',
+    '/img/icons/icon-128x128.png',
+    '/img/icons/icon-144x144.png',
+    '/img/icons/icon-152x152.png',
+    '/img/icons/icon-192x192.png',
+    '/img/icons/icon-384x384.png',
+    '/img/icons/icon-512x512.png'
 ];
 self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
